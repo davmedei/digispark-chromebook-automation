7259632e590af282de3570dc236df144c2031310
davmedei <17586937+davmedei@users.noreply.github.com>
2021-04-21 17:45:22 -1000

    added variables for delay and wifi credentials

--- dell_chromebook_3100/chrome_86_enroll/chrome_86_enroll.ino
+++ dell_chromebook_3100/chrome_86_enroll/chrome_86_enroll.ino
@@ -4,85 +4,103 @@ void setup() {}
 
 void loop() {
 
+  // DELAY VARIABLES
+  // IN MILLISECONDS
+  // 1000 MILLISECONDS IS 1 SECOND
+  
+  int SHORTDELAY, MEDIUMDELAY, LONGDELAY;
+  SHORTDELAY = 500;
+  MEDIUMDELAY = 10000;
+  LONGDELAY = 20000;
+
+  // WIFI CONNECTION VARIABLES
+  // UPDATE 'YOUR-WIFI-SSID' AND 'YOUR-WIFI-PASSWORD'
+  
+  char WIFISSID[] = "YOUR-WIFI-SSID";
+  char WIFIPASSWORD[] = "YOUR-WIFI-PASSWORD";
+
   // CHROME OS VER 86 ENROLLMENT
   
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(MEDIUMDELAY);
 
   // CONNECT TO WIFI
   
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(MEDIUMDELAY);
   DigiKeyboard.sendKeyStroke(KEY_S,MOD_ALT_LEFT|MOD_SHIFT_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(4000);
-  DigiKeyboard.print("WIFISSID");
+  DigiKeyboard.delay(MEDIUMDELAY);
+  DigiKeyboard.print(WIFISSID);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(81);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(81);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("WIFIPASSWORD");
+  DigiKeyboard.delay(SHORTDELAY);
+  DigiKeyboard.print(WIFIPASSWORD);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.delay(MEDIUMDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(20000);
+  DigiKeyboard.delay(LONGDELAY);
   
   // AGREE TERMS OF SERVICE
   
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(43);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(SHORTDELAY);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(20000);
+  DigiKeyboard.delay(LONGDELAY);
   
   // ENTERPRISE ENROLLMENT LOGIN
   
   DigiKeyboard.sendKeyStroke(KEY_E, MOD_ALT_LEFT|MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.delay(MEDIUMDELAY);
   DigiKeyboard.print("ENROLLUSERNAME");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.delay(MEDIUMDELAY);
   DigiKeyboard.print("ENROLLPASSWORD");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100000000);
+
+  // END OF TASK
+  
+  DigiKeyboard.delay(10000000000);
 }
